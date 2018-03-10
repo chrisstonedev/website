0377745e235e723e0ccb9b591a339c1ff9c5aa56
Christopher Stone <christopher@closetoeuphoria.com>
2018-03-09 21:48:05 -0500

    Partial follow along

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContentComponent } from './content/content.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    ListComponent,
+    ItemComponent,
+    MenuComponent,
+    ContentComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
