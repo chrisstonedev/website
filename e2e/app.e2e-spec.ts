184dccc2f9c8aaa9fb5f2cc5863ad6d8776025e0
Christopher Stone <christopher@closetoeuphoria.com>
2018-04-08 14:38:13 -0400

    Minor fixes to tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('personal-website App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Christopher Stone');
   });
 });
--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MenuComponent } from './menu.component';
+import { HeaderComponent } from './header.component';
 
-describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      declarations: [ HeaderComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MenuComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
