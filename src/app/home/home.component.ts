c1762f2e0ede67543e8bee5cb9aeb82af328151e
Chris Stone <contact@chrisstone.dev>
2021-09-23 00:54:11 -0400

    Added Atlanta Code Camp banner on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { animate, AnimationBuilder, group, query, style } from '@angular/animati
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   currentTaglineIndex = -1;
