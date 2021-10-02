f041613e7fa8fe8f0172d8c9768c4c441aa01a85
Chris Stone <contact@chrisstone.dev>
2021-10-02 15:36:05 -0400

    Migrated Protractor tests to Cypress

--- cypress/integration/home_page.spec.ts
+++ cypress/integration/home_page.spec.ts
@@ -1,6 +1,15 @@
 describe('Home Page Tests', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should display the correct title', () => {
+    cy.get('.navbar-brand').should((text) => {
+      expect(text).to.have.text('Chris Stone');
+    });
+  });
+
   it('should display Atlanta Code Camp banner ad', () => {
-    cy.visit('http://localhost:4200');
     cy.get('.atlanta-code-camp-title').should('exist');
   })
 })
---
+++ cypress/integration/projects.spec.ts
@@ -0,0 +1,28 @@
+describe('Projects List Page', () => {
+  beforeEach(() => {
+    cy.visit('/projects');
+  });
+
+  it('should have an unordered list', () => {
+    cy.get('ul').should('exist');
+  });
+
+  it('should have 13 projects in the default list', () => {
+    cy.get('app-item').should('have.length', 13);
+    cy.get('#flexCheckDefault').should('not.be.checked');
+  });
+
+  it('should have 6 projects in the filtered list', () => {
+    cy.get('#flexCheckDefault').check({scrollBehavior: false});
+    cy.get('app-item').should('have.length', 6);
+    cy.get('#flexCheckDefault').should('be.checked');
+  });
+
+  it('should have 13 projects after unfiltering again', () => {
+    const filterCheckBox = cy.get('#flexCheckDefault');
+    filterCheckBox.click({scrollBehavior: false});
+    filterCheckBox.click({scrollBehavior: false});
+    cy.get('app-item').should('have.length', 13);
+    filterCheckBox.should('not.be.checked');
+  });
+});
