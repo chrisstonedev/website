ad814589af4b0ec9f089b6023dcdeb80bb7fd17d
Chris Stone <contact@chrisstone.dev>
2022-05-24 22:40:10 -0400

    Routing to new pages scrolls to the top

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,23 +8,27 @@ import {ContactComponent} from './contact/contact.component';
 import {ProjectDetailComponent} from './portfolio/project-detail/project-detail.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SpeakingComponent} from './speaking/speaking-list/speaking.component';
-import {SpeakingTalkComponent} from "./speaking/speaking-talk/speaking-talk.component";
+import {SpeakingTalkComponent} from './speaking/speaking-talk/speaking-talk.component';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      {path: '', component: HomeComponent, data: {animationState: 'Home'}},
-      {path: 'work', component: WorkComponent, data: {animationState: 'Work'}},
-      {path: 'portfolio', component: ProjectsListComponent, data: {animationState: 'Portfolio'}},
-      {path: 'portfolio/:slug', component: ProjectDetailComponent, data: {animationState: 'Portfolio'}},
-      {path: 'speaking', component: SpeakingComponent, data: {animationState: 'Speaking'}},
-      {path: 'speaking/:slug', component: SpeakingTalkComponent, data: {animationState: 'Speaking'}},
-      {path: 'contact', component: ContactComponent, data: {animationState: 'Contact'}},
-      {path: '**', redirectTo: '/'}
-    ]),
-    BrowserAnimationsModule
+    RouterModule.forRoot(
+      [
+        {path: '', component: HomeComponent, data: {animationState: 'Home'}},
+        {path: 'work', component: WorkComponent, data: {animationState: 'Work'}},
+        {path: 'portfolio', component: ProjectsListComponent, data: {animationState: 'Portfolio'}},
+        {path: 'portfolio/:slug', component: ProjectDetailComponent, data: {animationState: 'Portfolio'}},
+        {path: 'speaking', component: SpeakingComponent, data: {animationState: 'Speaking'}},
+        {path: 'speaking/:slug', component: SpeakingTalkComponent, data: {animationState: 'Speaking'}},
+        {path: 'contact', component: ContactComponent, data: {animationState: 'Contact'}},
+        {path: '**', redirectTo: '/'},
+      ], {
+        scrollPositionRestoration: 'enabled',
+      },
+    ),
+    BrowserAnimationsModule,
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {
 }
